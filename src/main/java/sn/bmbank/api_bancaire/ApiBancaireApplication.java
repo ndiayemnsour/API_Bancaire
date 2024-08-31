9213046712d68f86f34eab97aad74b275ad3a499
ndiayemnsour <zocod3r@gmail.com>
2024-08-31 01:22:08 +0000

    Depot du projet

---
+++ src/main/java/sn/bmbank/api_bancaire/ApiBancaireApplication.java
@@ -0,0 +1,13 @@
+package sn.bmbank.api_bancaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/bmbank/api_bancaire/ApiBancaireApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.bmbank.api_bancaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
