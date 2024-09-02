22247173d69cea731b5a50f8840d080ec270be5d
ndiayemnsour <zocod3r@gmail.com>
2024-09-02 05:37:20 +0000

    Implementation du model Statut

--- src/main/java/sn/bmbank/api_bancaire/model/Statut.java
+++ src/main/java/sn/bmbank/api_bancaire/model/Statut.java
@@ -1,5 +1,32 @@
 package sn.bmbank.api_bancaire.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "statut")
 public class Statut {
+    @Id
+    private String id_statut;
+    @Getter
+    @Setter
+    private String nom_statut;
+
+
+
+    public Statut(){
+
+    }
     
+    // Générer un ID de 6 caractères basé sur UUID avant de persister l'entité
+    @PrePersist
+    public void generateShortUUID() {
+        this.id_statut= UUID.randomUUID().toString().substring(0, 6);
+    }
 }
